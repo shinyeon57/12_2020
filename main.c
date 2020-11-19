f01068d550aef609b0cc1323c5d07944ede87ab0
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-11-19 16:17:45 +0900

    practice 06

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void){
-	int	i;
-	FILE *fp;
-	char str[100];
-	
-	fp = fopen("sample.txt", "w");
-	if(fp==NULL)
-	{
-		return -1;
-	}
-	
-	for(i=0;i<3;i++)
-	{
-	
-	printf("Input a word :");
-	scanf("%s", str);
-	
-	fprintf(fp, "%s", str);
-}
-	
-	fclose(fp);
+int main(void) {
+   FILE *fp;
+   //char c;
+   char str[100];
+   
+   fp = fopen("sample. txt", "r");
+   if (fp == NULL)
+   {
+      return -1;
+   }
+   
+   //while((c = fgetc(fp)) != EOF ){
+   //   printf("%c",c);
+   //}
+   while(fgets(str, 100, fp) != NULL){
+      printf("%s", str);
+   }
+   
+   fclose(fp);
 
+   return 0;
 }
